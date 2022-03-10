051f931db6eb1ee378fda7b526f61dd680a340a4
Đào Thị Trúc Loan <67361437+loan1@users.noreply.github.com>
2022-03-10 20:24:42 +0700

    update k-fold

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from script.utils import *
 from script.dataset import *
 from script.visualize import *
+from script.model import *
 
 #importing the libraries
 import os
@@ -18,13 +19,14 @@ import torch
 from torchsummary import summary
 
 from torch.optim import Adam, lr_scheduler
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 
 #torchvision for pre-trained models and augmentation
 
 #for evaluating model
 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 import argparse
 
 from torch.nn import BCEWithLogitsLoss
@@ -37,8 +39,8 @@ def get_opt():
     parser.add_argument('--mask_path', default='./dataset_lungseg/masks/', type= str)
 
     parser.add_argument('--BATCH_SIZE', default=8, type=int)
-    parser.add_argument('--num_epochs', default= 50, type=int)
-    parser.add_argument('--lr', default=1e-4, type=float)
+    parser.add_argument('--num_epochs', default= 15, type=int)
+    parser.add_argument('--lr', default=1e-3, type=float)
 
     opt = parser.parse_args()
     return opt
@@ -53,12 +55,13 @@ def dataloader():
     img_mask_list = [(mask_names.replace('_mask',''), mask_names) for mask_names in mask_list]
 
     train_list, val_list = train_test_split(img_mask_list, test_size = 0.2, random_state = 42) 
+
     # train_list, val_list = train_test_split(train_list,test_size = 0.1, random_state = 42)
     # print(len(train_list), len(val_list), len(test_list)) # 506, 57, 141
     # print(train_list)
 
     aug = A.Compose([
-        A.Resize(224, 224), 
+        A.Resize(256, 256), 
         A.HorizontalFlip(p=0.5),
         A.OneOf([
             A.RandomGamma(),
@@ -71,7 +74,7 @@ def dataloader():
     ])
 
     transfm = A.Compose([
-        A.Resize(224,224),
+        A.Resize(256,256),
         A.Normalize(mean = [0.5],  std = [0.5]),
         ToTensorV2()
     ])
@@ -83,12 +86,12 @@ def dataloader():
     loader ={
         'train' : DataLoader(
             train_set, 
-            batch_size= 4,
+            batch_size= 8,
             shuffle=True
         ),
         'val' : DataLoader(
             val_set, 
-            batch_size=4,
+            batch_size=8,
             shuffle=True
         ),
         # 'test' : DataLoader(
@@ -99,6 +102,20 @@ def dataloader():
     }   
     return loader
 
+def datasetKfold(kfold):
+
+    mask_path = './dataset_lungseg/masks/'
+    img_path = './dataset_lungseg/images/'
+    mask_list = os.listdir(mask_path)
+    img_mask_list = [(mask_names.replace('_mask',''), mask_names) for mask_names in mask_list]
+    
+    idx = kfold.split(img_mask_list) 
+    # dataset = LungDataset(img_mask_list, img_path, mask_path, ToTensorV2())
+    return idx, img_mask_list
+
+def get_item_to_idx(idx_list,image_list):
+  train_list = [image_list[i] for i in idx_list]
+  return train_list
 
 def main():
     opt = get_opt()
@@ -148,9 +165,122 @@ def main():
     # plt.imshow(ynum[0], cmap='gray')
     # plt.show()
 
+def mainKFold():
+    opt = get_opt()
+    aug = A.Compose([
+        A.Resize(256, 256), 
+        A.HorizontalFlip(p=0.5),
+        A.OneOf([
+            A.RandomGamma(),
+            A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+            A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=0.9),            
+        ], p = 0.3),
+        A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=30, p=0.5),         
+        A.Normalize(mean = [0.5],  std = [0.5]),
+        ToTensorV2()   
+    ])
+
+    transfm = A.Compose([
+        A.Resize(256,256),
+        A.Normalize(mean = [0.5],  std = [0.5]),
+        ToTensorV2()
+    ])
+
+    k_folds = 5
+    # For fold results
+    results = {}
+    # Set fixed random number seed
+    torch.manual_seed(42)
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    loss_fn = ComboLoss()
+
+    torch.cuda.empty_cache()
+
+    # train and validation
+
+    ###########################
+    ############K-FOLD#########
+    ###########################
+    #define the K-fold Cross Validator
+    kfold = KFold(n_splits=k_folds, shuffle=True)
+    idx, img_mask_list = datasetKfold(kfold)
+
+    # print(next(iter(idx)))
+    #Start print
+    print('=======================================')
+
+    for fold, (train_ids, test_ids) in enumerate(idx): #goi vay no sai
+
+        #Print
+        print(f'FOLD {fold}')
+        print('-------------------------')
+        train_ids = get_item_to_idx(train_ids, img_mask_list)
+        test_ids  = get_item_to_idx(test_ids, img_mask_list)
+
+        # print(len(train_ids))
+        # print (len(test_ids))
+
+        train_subsampler = LungDataset(train_ids,opt.img_path, opt.mask_path,aug)  
+        test_subsampler = LungDataset(test_ids,opt.img_path, opt.mask_path,transfm)
+
+        trainloader = DataLoader(
+            train_subsampler,
+            batch_size=8,
+            shuffle=True
+        )
+
+        testloader = DataLoader(
+            test_subsampler,
+            batch_size=8,
+            shuffle=True
+        )
+
+        model = UNet_ResNet.to(device)
+        model.apply(reset_weights)
+        optimizer = Adam(model.parameters(), opt.lr)
+        # scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+        scheduler3 = lr_scheduler.CosineAnnealingLR(optimizer, T_max = 10)
+        # loss_fn = DiceBCELoss()
+        # loss_fn = BCEWithLogitsLoss()
+        loss_fn = ComboLoss()
+        
+        res = fit(model, trainloader, testloader, optimizer, scheduler3, opt.num_epochs, loss_fn, calculate_metrics, opt.CHECKPOINT_PATH, device)
+    
+        # visualize loss, acc
+        loss, val_loss = res['loss'], res['val_loss']
+        acc, val_acc = res['acc'], res['val_acc']
+
+        Accuracy = 0
+        for k in range(len(val_acc)):
+            Accuracy +=  val_acc[k]
+        
+        # Print accuracy
+        print ('Accuracy for fold %d: %f %%' % (fold, 100* Accuracy/len(val_acc)))
+        print('--------------------------------')
+        results[fold] = 100* Accuracy/len(val_acc)
+
+      # Print fold results
+    print(f'K-FOLD CROSS VALIDATION RESULTS FOR {k_folds} FOLDS')
+    print('--------------------------------')
+    sum = 0.0
+    for key, value in results.items():
+        print(f'Fold {key}: {value} %')
+        sum += value
+    print(f'Average: {sum/len(results.items())} %')
+
+
+##################################################################################
+    plot_acc_loss (loss, val_loss, acc, val_acc, './visualize/loss_acc')
+
+    plot_LR(res['learning_rate'], './visualize/LR')
+
+    print(res['learning_rate'])
+  
 
 if __name__ == '__main__':
-    main()
+    mainKFold()
     # dataloader1()
     # image, mask = next(iter(dataloader()['train']))
 
