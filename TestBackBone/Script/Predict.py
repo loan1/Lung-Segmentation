37560026458a17974de16fa3a52e46ffb79c6027
Đào Thị Trúc Loan <67361437+loan1@users.noreply.github.com>
2022-03-31 11:36:49 +0700

    update

--- TestBackBone/Script/Predict.py
+++ TestBackBone/Script/Predict.py
@@ -1,7 +1,9 @@
 from models import *
-from dataset import DatasetPredict, LungDataset
-from utils import calculate_metrics
-from operator import add
+from dataset import DatasetPredict
+from PIL import Image
+import cv2
+from skimage import morphology
+from tqdm import tqdm
 
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
@@ -22,8 +24,8 @@ def dataloaderPre(): # data 14gb
         ToTensorV2()   
     ])
 
-    dataNegative = DatasetPredict(data_dir + 'Negative50/', transform = transfms)
-    dataPositive = DatasetPredict(data_dir + 'Positive50/', transform = transfms)
+    dataNegative = DatasetPredict(data_dir + 'Negative/', transform = transfms)
+    dataPositive = DatasetPredict(data_dir + 'Positive/', transform = transfms)
 
     loader ={
         'Negative' : DataLoader(
@@ -45,8 +47,8 @@ def dataloaderPre(): # data 14gb
 def predict(dataloader, model, device): # dataset 14gb
     model.eval()
     with torch.no_grad():
-        original, image, y_predict = [], [], []
-        for x in dataloader:
+        image, y_predict = [], []
+        for x, name in tqdm(dataloader):
             # x = x.to(device, dtype=torch.float32)
             x = x.to(device)
             # x = x.cpu().numpy()        
@@ -81,13 +83,13 @@ def predict(dataloader, model, device): # dataset 14gb
     return image, y_predict
 
 def mainPredict():
-    cp_list = ['../../model/UNetResNet18/UNet0.pt', '../../model/UNetResNet18/UNet1.pt', '../../model/UNetResNet18/UNet2.pt', '../../model/UNetResNet18/UNet3.pt', '../../model/UNetResNet18/UNet4.pt']
+    cp_list = ['../../model/UNetResNet18Gauss/UNet0.pt', '../../model/UNetResNet18Gauss/UNet1.pt', '../../model/UNetResNet18Gauss/UNet2.pt', '../../model/UNetResNet18Gauss/UNet3.pt', '../../model/UNetResNet18Gauss/UNet4.pt']
     
     y = []
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     modelUNet = UNet_ResNet18.to(device)
     
-    for fold in range(len(cp_list)):
+    for fold in tqdm(range(len(cp_list))):
         checkpoint = torch.load(cp_list[fold])
         modelUNet.load_state_dict(checkpoint)
 
@@ -97,7 +99,7 @@ def mainPredict():
         # img = img['image']
         # img = img.unsqueeze(0)
 
-        x, y_pred = predict(dataloaderPre()['Positive'], modelUNet, device)
+        x, y_pred = predict(dataloaderPre()['Negative'], modelUNet, device)
         
         # print('x: ', x.shape)
         # print('y_pred: ', y_pred.shape)
@@ -106,7 +108,7 @@ def mainPredict():
     y_mean = np.mean(np.stack(y, axis=0), axis=0) 
 
     for idx in range(len(y_mean)):
-        y_mean[idx] = y_mean[idx] > 0.3
+        y_mean[idx] = y_mean[idx] > 0.1
         y_mean[idx] = y_mean[idx].astype(np.uint8)
 
     # print('x.shape ',x.shape)
@@ -114,52 +116,38 @@ def mainPredict():
     # print('y_mean.shape ',y_mean.shape)
 
 
-    imshowPre(x, y, y_mean, '../../visualize/InferResNet18Gauss/InferResNet18.png')
+    # imshowPre(x, y, y_mean, '../../visualize/InferPositive/InferResNet18GaussPreFULL/InferResNet18.png')
 
-def imshowPre(x ,pred, mean, path):
+    np.save('../../visualize/InferResNet18GaussPre_NegFULL/images.npy',x)
+    np.save('../../visualize/InferResNet18GaussPre_NegFULL/y_predict_5folds.npy',y)
+    np.save('../../visualize/InferResNet18GaussPre_NegFULL/y_predict_mean_5folds.npy',y_mean)
 
-    for i in range(13):
+    # plt.show()
+def postprocess(img):
+    # img = cv2.imread(img_name, 0)
+    areas = []
+    img = img.astype("uint8")
+    blur = cv2.GaussianBlur(img, (3,3), 0) #làm mờ ảnh
+    _, thresh = cv2.threshold(blur, 0,1, cv2.THRESH_BINARY + cv2.THRESH_OTSU) #nhị phân hóa ảnh
 
-        plt.figure (figsize = (15, 20))
+    contours, hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #tính contours
 
-        for idx in range(4):
+    for i in range(len(contours)):
+        areas.append(cv2.contourArea(contours[i]))
+    areas = np.sort(areas)[::-1]
 
-            plt.subplot (4,7,idx*7 +1)
-            # print(original.shape)
-            plt.imshow(x[i][idx], cmap='gray') 
+    thresh = thresh.astype(bool)
+    if len(contours) > 1:
+        thresh = morphology.remove_small_objects(thresh.copy(),areas[1])
+    if len(contours) > 2 :
+        thresh = morphology.remove_small_holes(thresh, areas[2])
+    
+    return thresh
 
-            plt.subplot (4,7,idx*7 +2)
-            plt.imshow(pred[0][i][idx], cmap='gray')
-            # plt.title('Predict Mask')
-
-            plt.subplot (4,7,idx*7 +3)
-            plt.imshow(pred[1][i][idx], cmap='gray')
-            # plt.title('Predict Mask')
-
-            plt.subplot (4,7,idx*7 +4)
-            plt.imshow(pred[2][i][idx], cmap='gray')
-            # plt.title('Predict Mask')
-
-            plt.subplot (4,7,idx*7 +5)
-            plt.imshow(pred[3][i][idx], cmap='gray')
-            # plt.title('Predict Mask')
-
-
-            plt.subplot (4,7,idx*7 +6)
-            plt.imshow(pred[4][i][idx], cmap='gray')
-            # plt.title('Predict Mask')
-
-            plt.subplot (4,7,idx*7 +7)
-            # print(mean.shape)
-            plt.imshow(mean[i][idx], cmap='gray')
-            # plt.title('Predict Mean')
-            
-        plt.savefig(path.replace('.png', '_' +str(i) + '.png'))
-        plt.show()
 
 if __name__ == '__main__':
 
-    mainPredict()
+    # mainPredict()
     ############################################################################
     # data_dir = '../../dataset_lungseg/predict/'
     # img_folder = data_dir + 'Negative50/'
@@ -172,8 +160,61 @@ if __name__ == '__main__':
     # # print('images: ', images)
     # images = np.array(images, dtype=np.float32)
     # print('images: ', images.shape)
-    # img = next(iter(dataloaderPre()['Positive']))
-    # # print(img)
+    # img, name = next(iter(dataloaderPre()['Positive']))
+    # # print(name)
+    # for i in range(4):
+    #     plt.imshow(img[i][0], cmap='gray')
+    #     plt.title(name[i])
+    #     plt.show()
+    # print(img)
     # for i in range(len(img)):
     #     plt.imshow(img[i][0], cmap='gray')
     #     plt.show()
+
+
+    #######################################################################################################
+    ###################################
+    ### TAO FILE TXT LUU TEN ANH #####
+    ##################################
+    # list_name =[]
+    # for img,name in tqdm(dataloaderPre()['Positive']):
+    #     list_name.append(name)
+    # # print(len(list_name)) # 4123
+    # # print(list_name)
+
+    # res = []
+    # for i in tqdm(range(len(list_name)-1)):
+    #     for idx in range(4):
+    #         res.append(list_name[i][idx])
+    
+    # for i in range(2): #xu li phan le trong batch cuoi
+    #     res.append(list_name[len(list_name)-1][i])
+
+    # # print(res)
+
+    # np.savetxt('../../dataset_lungseg/predict/filenamePos.txt', res, fmt = '%s')
+    ########################################################################################################
+
+          # print(y_mean.shape) # y_mean = 4123 x 4 x 256 x 256
+    
+    # ######################################################################################
+
+    images_np = np.load('../../visualize/InferResNet18GaussPre_NegFULL/images.npy', allow_pickle=True)
+    y = np.load('../../visualize/InferResNet18GaussPre_NegFULL/y_predict_5folds.npy', allow_pickle=True)
+    y_mean = np.load('../../visualize/InferResNet18GaussPre_NegFULL/y_predict_mean_5folds.npy', allow_pickle=True)
+
+    list_name = np.loadtxt('../../dataset_lungseg/predict/filenameNeg.txt', dtype = list)
+    for i in tqdm(range(len(dataloaderPre()['Negative']))):   # 
+        for idx in range(4):
+
+            ret = postprocess(y_mean[i][idx])
+
+            plt.imsave('../../dataset_lungseg/predict/NegMask/' + list_name[i*4+idx], ret)
+
+    plt.close('all')
+
+
+
+
+
+    
